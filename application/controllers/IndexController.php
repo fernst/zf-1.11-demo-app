7bf3bb8371369d418b10e750c2423a9f68b58880
Fernando Velasquez <fvelasquez3000@gmail.com>
2014-07-26 16:47:02 -0400

    Configured Form, created the Index action and both index and result views.

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -3,14 +3,84 @@
 class IndexController extends Zend_Controller_Action
 {
 
-    public function init()
-    {
-        /* Initialize action controller here */
-    }
-
     public function indexAction()
     {
-        // action body
+        $request = $this->getRequest();
+        $csvForm = new Application_Form_Input();
+		
+		//List of errorsm user to gracefully display application errors
+		$errors = array();
+ 		
+		//If a form has been submitted, attempt to process CSV File.
+        if ($this->getRequest()->isPost()) {
+        	//Check if Form was validated successfully
+            if ($csvForm->isValid($request->getPost())) {
+            	
+				//Get the Email Address from the Form
+				$email = $csvForm->getValue('text');
+				
+            	//Check if file was received successfully. If not, application ends with an error message.
+				if (!$csvForm->file->receive()) {
+				    $errors[] = "Error Receiving the File";
+				} else {										
+					//Get File location
+					$location = $csvForm->file->getFileName();				
+					try {
+						//Open and read first line of file, looking for the Firstname column.					
+						$file = fopen($location,"r");
+						
+						//If the file was not opened successfully, display an error
+						if ($file===false) {
+							throw new Exception('Error reading uploaded file');
+						};
+						
+						//Read headers from the CSV file and find the Firstname key.
+						$headers = fgetcsv($file);
+						$key = array_search('Firstname',$headers);
+						
+						//If the First Name key is not found, file format is not valid.
+						if ($key===false) {
+							throw new Exception('File Format is not valid');
+						};
+						
+						//Fill an array with the list of people. Also, get the list of first names for sorting.
+						$peopleList = array();	
+						$sortIndex = array();
+						
+						//Populate peopleList array with the remaining CSV Lines.
+						while(!feof($file))
+						{
+							$line = fgetcsv($file);
+							if ($line===false) continue;
+							$peopleList[] = $line;
+							$sortIndex[] = $line[$key];
+						}
+						
+						//Sort the peopleList array using the list of names in ascending order.
+						array_multisort(
+							$sortIndex,SORT_ASC,$peopleList
+						);
+						
+						//Close the file.
+						fclose($file);
+						
+						//Set the view variables
+						$this->view->email = $email;
+						$this->view->peopleList = $peopleList;
+						
+						//Render the result view.
+						$this->_helper->viewRenderer('index/result', null, true);
+						
+					} catch (Exception $e) {
+				    	$errors[] = "Error processing the file: ".$e->getMessage();
+					}
+				}	
+            }
+        }
+ 
+ 		//Set the form and the error array for the index view.
+        $this->view->form = $csvForm;
+		$this->view->errors = $errors;
     }
 
 
---
+++ application/forms/Input.php
@@ -0,0 +1,72 @@
+<?php
+
+class Application_Form_Input extends Zend_Form
+{
+
+    public function init()
+    {
+        // Set the method for the form to POST
+        $this->setMethod('post');
+		
+		// Set this form as a File Upload form
+		$this->setAttrib('enctype', 'multipart/form-data');
+		
+		// Add an text element for the Email Address
+		$email = new Zend_Form_Element_Text('text');
+		
+		// Specify Label for the field
+		$email->setLabel('Your email address:')
+    	  ->setRequired(true);
+		
+		// Add validation for the form
+		$email->addValidator('NotEmpty', true)
+			->addValidator('EmailAddress', true)
+			->addFilter('StringTrim');
+		
+		// Customize Error Messages	
+		$email->getValidator('NotEmpty')
+			 ->setMessage('Please specify an Email Address');
+		$email->getValidator('EmailAddress')
+			 ->setMessage('Please type a valid Email Address');
+		
+		// Add this element to Form
+		$this->addElement($email, 'text');
+		
+		
+		// Create a new File Form Element
+		$file = new Zend_Form_Element_File('file');
+		$file->setLabel('CSV File:')
+			->setDestination(sys_get_temp_dir())
+    		->setRequired(true);
+		
+		// Ensure a file is always selected to upload		
+		$file->addValidator('File_Upload', true);
+		
+		// ensure only 1 file
+		$file->addValidator('Count', true, 1);
+		
+		// only CSV Files
+		$file->addValidator('Extension', true, 'csv');
+		
+		//Customize Error Messages
+		$file->getValidator('File_Upload')
+			 ->setMessage('Please select a CSV File');
+		$file->getValidator('Count')
+			 ->setMessage('Select only 1 File');
+		$file->getValidator('Extension')
+			 ->setMessage('Only CSV files are allowed');
+			 
+		// Add this element to Form	 
+		$this->addElement($file, 'file');
+ 	 
+			 
+        // Add the submit button
+        $this->addElement('submit', 'submit', array(
+            'ignore'   => true,
+            'label'    => 'Upload File',
+        ));
+    }
+
+
+}
+
